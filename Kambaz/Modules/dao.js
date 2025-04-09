5b1705c43fc2ab0fbaf186ef7b62311df3de88d1
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-04-09 14:02:23 -0400

    update module

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -16,9 +16,6 @@ export function deleteModule(moduleId) {
 }
 
 export function updateModule(moduleId, moduleUpdates) {
-    const { modules } = Database;
-    const module = modules.find((module) => module._id === moduleId);
-    Object.assign(module, moduleUpdates);
-    return module;
+    return model.updateOne({ _id: moduleId }, moduleUpdates);
 }
 
